d7a2865d54eb579ec287bf09857485914e3702ea
robles153 <marcoskim153@gmail.com>
2022-02-01 23:14:54 -0300

    Adicionar arquivos de projeto.

---
+++ Crud/Models/BamcoDeDados.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Models
+{
+
+    public class BamcoDeDados :DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Crud;Integrated Security=True");
+        }
+    }
+}
